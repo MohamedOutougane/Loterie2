f2ac60c062df3575d941c9c2524ecd6995c6403a
Outougane Mohamed <outougane.moh@gmail.com>
2022-12-06 21:11:48 +0100

    scaffolding of the models ( controllers, data folder and views)

---
+++ Loterie/Data/LoterieContext.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Loterie.Models;
+
+namespace Loterie.Data
+{
+    public class LoterieContext : DbContext
+    {
+        public LoterieContext (DbContextOptions<LoterieContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Loterie.Models.Session> Session { get; set; } = default!;
+
+        public DbSet<Loterie.Models.Ticket> Ticket { get; set; } = default!;
+    }
+}
