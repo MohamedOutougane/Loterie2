12013f46eb5b859d2e2ecb6d79277ed9ae229081
Outougane Mohamed <outougane.moh@gmail.com>
2022-12-06 21:11:01 +0100

    creation of the models

---
+++ Loterie/Models/Session.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Loterie.Models
+{
+    public class Session
+    {
+        public int Id { get; set; }
+        public string? Draw { get; set; }
+        [DataType(DataType.Date)]
+        public DateTime Date { get; set; }
+        public int Jackpot { get; set; }
+
+        public ICollection<Ticket>? Tickets { get; set; }
+    }
+}
