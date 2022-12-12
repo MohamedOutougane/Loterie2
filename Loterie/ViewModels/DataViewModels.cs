55d2e422640a2ee5ad7738bd82af2e73c4d917c0
Outougane Mohamed <outougane.moh@gmail.com>
2022-12-12 15:04:56 +0100

    ok

---
+++ Loterie/ViewModels/DataViewModels.cs
@@ -0,0 +1,12 @@
+﻿using Loterie.Models;
+using Loterie.Data;
+
+namespace Loterie.ViewModels
+{
+    public class DataViewModels
+    {
+        public Session? Session { get; set; }
+        public Ticket? Ticket { get; set; }
+
+    }
+}
